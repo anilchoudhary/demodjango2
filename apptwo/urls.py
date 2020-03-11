28afb7110e27c7e933d3d358513837e757de4abb
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-11 23:47:09 +0530

    added model forms

--- apptwo/urls.py
+++ apptwo/urls.py
@@ -2,6 +2,6 @@ from apptwo import views
 from django.urls import path
 
 urlpatterns = [
-    path('index/', views.help),
+    path('index/', views.index),
     path('users/', views.users, name = 'users'),
 ]
--- apptwo/views.py
+++ apptwo/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from apptwo.models import User
+# from django.http import HttpResponse
+from apptwo.forms import NewUserForm
 # Create your views here.
 
 
-def help(request):
-    my_dict = {'insert_me': "please use users/ in URL "}
-    return render(request, 'apptwo/help.html', context=my_dict)
+def index(request):
+    return render(request, 'apptwo/index.html')
 
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'user': user_list}
-    return render(request, 'apptwo/user.html', context=user_dict)
+
+    form = NewUserForm()
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FORM INVALID')
+
+    return render(request, 'apptwo/user.html', {'form':form})
